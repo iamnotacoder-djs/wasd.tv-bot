a15b26c188a815ad7f2060867215798b57b378ed
idaspin <3via.idaspin@gmail.com>
2022-05-13 17:55:33 +0300

    package

---
+++ WASD.js
@@ -0,0 +1,319 @@
+'use strict';
+
+const io = require('socket.io-client'),
+      request = require('request'),
+      EventEmitter = require('events');
+
+// https://wasd.tv/faq/dev
+class WASD {
+
+    event = new EventEmitter();
+
+    #JWT_TOKEN = ``;
+    #ACCESS_TOKEN = ``;
+    userName = ``;
+    userId = 0;
+    channelId = 0;
+    streamId = 0;
+  
+    #socket = ``;
+
+    constructor() {
+    }
+
+    async init(userName = ``, access_token = ``, streamId) {
+        this.userName = userName;
+        this.#ACCESS_TOKEN = access_token;
+        this.#JWT_TOKEN = await this.#getJWTToken();
+        this.channelId = await this.#getChannelId(this.userName);
+        this.userId = await this.#getUserId(this.userName);
+        this.streamId = await this.#getStreamId(this.channelId);
+        this.streamId = streamId ?? this.streamId;
+
+        console.log(`Trying to connect:\n${this.userName} (ID: ${this.channelId}) | StreamID: ${this.streamId}`);
+        this.#socket = io('wss://chat.wasd.tv', {
+            transports: ["websocket"],
+            query: {
+                path: '/socket.io',
+                EIO: 3
+            }
+        });
+      
+        this.#socket.on("connect_error", (err) => {
+            this.event.emit('error', `APIConnectError`, err);
+            console.log(`I'll try to reconnect in 10 seconds.`);
+            setTimeout(() => {
+              this.init();
+            }, 1000 * 10);
+        });
+      
+        this.#socket.on('connect', () => {
+            this.event.emit('ready', `APIConnect`);
+            this.#joinChat();
+        });
+      
+        this.#socket.on('message', (message) => {
+            this.event.emit('message', message);
+        });
+      
+        this.#socket.on('event', (event) => {
+            this.event.emit('follow', event);
+        });
+      
+        this.#socket.on('subscribe', (event) => {
+            this.event.emit('subscribe', event);
+        });
+      
+        this.#socket.on('close', () => {
+            this.event.emit('error', `APIDisconnect`);
+        });
+    }
+
+    #getJWTToken() {
+        return new Promise((resolve, reject) => {
+            var options = {
+                method: "GET",
+                url: "https://wasd.tv/api/auth/chat-token",
+                headers: {
+                   'Authorization': `Token ${this.#ACCESS_TOKEN}`
+                }
+            }
+            request(options, (error, response, body) => {
+                if (error) return reject();
+                resolve(JSON.parse(body).result);
+            });
+        });
+    }
+    
+    #getChannelId(username = this.userName) {
+        return new Promise((resolve, reject) => {
+            var options = {
+                method: "GET",
+                url: `https://wasd.tv/api/channels/nicknames/${username}`,
+                headers: {
+                   'Authorization': `Token ${this.#ACCESS_TOKEN}`
+                }
+            }
+            request(options, (error, response, body) => {
+                if (error) return reject();
+                resolve(JSON.parse(body).result.channel_id);
+            });
+        });
+    }
+    
+    #getUserId(username = this.userName) {
+        return new Promise((resolve, reject) => {
+            var options = {
+                method: "GET",
+                url: `https://wasd.tv/api/channels/nicknames/${username}`,
+                headers: {
+                   'Authorization': `Token ${this.#ACCESS_TOKEN}`
+                }
+            }
+            request(options, (error, response, body) => {
+                if (error) return reject();
+                resolve(JSON.parse(body).result.user_id);
+            });
+        });
+    }
+    
+    #getStreamId(channelid = this.channelId) {
+        return new Promise((resolve, reject) => {
+            var options = {
+                method: "GET",
+                url: `https://wasd.tv/api/v2/media-containers?limit=1&offset=0&media_container_status=RUNNING,STOPPED&media_container_type=SINGLE&channel_id=${channelid}`,
+                headers: {
+                   'Authorization': `Token ${this.#ACCESS_TOKEN}`
+                }
+            }
+            request(options, (error, response, body) => {
+                if (error) return reject();
+                resolve(JSON.parse(body).result[0].media_container_streams[0].stream_id);
+            });
+        });
+    }
+  
+    #joinChat() {
+        this.#socket.emit('join', {
+            streamId: this.streamId,
+            channelId: this.channelId,
+            jwt: this.#JWT_TOKEN,
+            excludeStickers: true
+        });
+        
+        this.#socket.on('joined', (opts) => {
+            console.log(`Joined to the chat with the options:`);
+            console.log(opts);
+            this.event.emit('ready', `chatJoin`, opts);
+            this.#pingServer();
+        });
+    }
+
+    #pingServer() {
+        this.#socket.emit('2');
+        setTimeout(() => {
+            this.#pingServer();
+        }, 1000 * 25);
+    }
+    
+    getChannelInfo(userName = this.userName) {
+        return new Promise((resolve, reject) => {
+            var options = {
+                method: "GET",
+                url: `https://wasd.tv/api/v2/broadcasts/public?channel_name=${userName}`,
+                headers: {
+                   'Authorization': `Token ${this.#ACCESS_TOKEN}`
+                }
+            }
+            request(options, (error, response, body) => {
+                if (error) return reject(error);
+                resolve(JSON.parse(body));
+            });
+        });
+    }
+    
+    getChannelSubs() {
+        return new Promise((resolve, reject) => {
+            var options = {
+                method: "GET",
+                url: `https://wasd.tv/api/chat/streams/${this.streamId}/messages?&type=SUBSCRIBE&limit=50&offset=0`,
+                headers: {
+                   'Authorization': `Token ${this.#ACCESS_TOKEN}`
+                }
+            }
+            request(options, (error, response, body) => {
+                if (error) return reject(error);
+                resolve(JSON.parse(body));
+            });
+        });
+    }
+    
+    setChatMode(chatRoleLimitMode = 0, chatDelayLimitMode = 0) {
+        if (![0, 1, 2].includes(chatRoleLimitMode)) chatRoleLimitMode = 0;
+        if (![0, 5, 10, 30, 60].includes(chatDelayLimitMode)) chatDelayLimitMode = 0;
+        return new Promise((resolve, reject) => {
+            var options = {
+                method: "POST",
+                url: `https://wasd.tv/api/chat/streams/${this.streamId}/settings`,
+                headers: {
+                   'Authorization': `Token ${this.#ACCESS_TOKEN}`
+                },
+                formData: {
+                    'chatRoleLimitMode': `${chatRoleLimitMode}`,
+                    'chatDelayLimitMode': `${chatDelayLimitMode}`
+                }
+            }
+            request(options, (error, response, body) => {
+                if (error) return reject(error);
+                resolve(JSON.parse(body));
+            });
+        });
+    }
+    
+    addModerator(userId = 0) {
+        return new Promise((resolve, reject) => {
+            var options = {
+                method: "PUT",
+                url: `https://wasd.tv/api/channels/${this.channelId}/moderators`,
+                headers: {
+                   'Authorization': `Token ${this.#ACCESS_TOKEN}`
+                },
+                formData: {
+                    'user_id': `${userId}`
+                }
+            }
+            request(options, (error, response, body) => {
+                if (error) return reject(error);
+                resolve();
+            });
+        });
+    }
+    
+    removeModerator(userId = 0) {
+        return new Promise((resolve, reject) => {
+            var options = {
+                method: "DELETE",
+                url: `https://wasd.tv/api/channels/${this.channelId}/moderators/${userId}`,
+                headers: {
+                   'Authorization': `Token ${this.#ACCESS_TOKEN}`
+                }
+            }
+            request(options, (error, response, body) => {
+                if (error) return reject(error);
+                resolve();
+            });
+        });
+    }
+    
+    banUser(userId = 0, keepMessages = true, minutes = 10) {
+        return new Promise((resolve, reject) => {
+            var options = {
+                method: "PUT",
+                url: `https://wasd.tv/api/channels/${this.channelId}/banned-users`,
+                headers: {
+                   'Authorization': `Token ${this.#ACCESS_TOKEN}`
+                },
+                formData: {
+                    'user_id': `${userId}`,
+                    'stream_id': `${this.streamId}`,
+                    'keep_messages': `${keepMessages}`,
+                    'duration': `${minutes}`
+                }
+            }
+            request(options, (error, response, body) => {
+                if (error) return reject(error);
+                resolve();
+            });
+        });
+    }
+    
+    unbanUser(userId = 0) {
+        return new Promise((resolve, reject) => {
+            var options = {
+                method: "DELETE",
+                url: `https://wasd.tv/api/channels/${this.channelId}/banned-users/${userId}`,
+                headers: {
+                   'Authorization': `Token ${this.#ACCESS_TOKEN}`
+                }
+            }
+            request(options, (error, response, body) => {
+                if (error) return reject(error);
+                resolve();
+            });
+        });
+    }
+    
+    getChannelModerators(userId = this.userId) {
+        return new Promise((resolve, reject) => {
+            var options = {
+                method: "GET",
+                url: `https://wasd.tv/api/chat/streamers/${userId}/moderators`,
+                headers: {
+                   'Authorization': `Token ${this.#ACCESS_TOKEN}`
+                }
+            }
+            request(options, (error, response, body) => {
+                if (error) return reject(error);
+                resolve(JSON.parse(body));
+            });
+        });
+    }
+    
+    getChannelBans(userId = this.userId) {
+        return new Promise((resolve, reject) => {
+            var options = {
+                method: "GET",
+                url: `https://wasd.tv/api/chat/streamers/${userId}/ban`,
+                headers: {
+                   'Authorization': `Token ${this.#ACCESS_TOKEN}`
+                }
+            }
+            request(options, (error, response, body) => {
+                if (error) return reject(error);
+                resolve(JSON.parse(body));
+            });
+        });
+    }
+}
+
+module.exports = new WASD();
---
+++ test-it.js
@@ -0,0 +1,85 @@
+const WASD = require('./WASD.js');
+
+// ACCESS_TOKEN: https://wasd.tv/general-settings/API
+WASD.init(`idoaspin`, `access_token`);
+
+WASD.event.on('ready', async (status) => {
+    let status_example = {
+        actions: [
+          'WRITE_TO_CHAT',
+          'WRITE_TO_FOLLOWERS_CHAT',
+          'WRITE_TO_SUBSCRIPTION_CHAT',
+          'VIEW_BANNED_USERS',
+          'ASSIGN_MODERATOR',
+          'BAN_USER',
+          'DELETE_MESSAGE',
+          'WRITE_NO_DELAY'
+        ],
+        user_channel_role: 'CHANNEL_OWNER',
+        other_roles: [],
+        user_sticker_packs: [],
+        chat_settings: { goodbyeBetaGifts: '0' }
+    };
+    if (status == "APIConnect") {
+        let info = await WASD.getChannelInfo();
+        console.log(info);
+        /* await WASD.getChannelSubs();
+        await WASD.setChatMode(chatRoleLimitMode, chatDelayLimitMode);
+        await WASD.addModerator(userId);
+        await WASD.removeModerator(userId);
+        await WASD.banUser(userId, keepMessages, minutes);
+        await WASD.unbanUser(userId);
+        await WASD.getChannelModerators(userId);
+        await WASD.getChannelBans(userId);*/
+    }
+});
+
+WASD.event.on('message', (message) => {
+    let message_example = {
+        id: '216dc720-c908-40dd-86af-979c6a744c29',
+        user_id: 1178907,
+        message: 'test',
+        user_login: 'idoaspin',
+        user_avatar: {
+          large: 'https://st.wasd.tv/upload/avatars/a1451e2d-4f2a-46d0-bf34-d1928491875d/original.jpeg',
+          small: 'https://st.wasd.tv/upload/avatars/a1451e2d-4f2a-46d0-bf34-d1928491875d/original.jpeg',
+          medium: 'https://st.wasd.tv/upload/avatars/a1451e2d-4f2a-46d0-bf34-d1928491875d/original.jpeg'
+        },
+        hash: 'cl339cin1000t399v12f48cy5',
+        is_follower: false,
+        other_roles: [],
+        user_channel_role: 'CHANNEL_OWNER',
+        channel_id: 1087064,
+        stream_id: 983499,
+        date_time: '2022-05-12T17:01:22.954Z',
+        streamer_id: 1178907
+    };
+});
+
+WASD.event.on('follow', (event) => {
+    let event_example = {
+        event_type: 'NEW_FOLLOWER',
+        id: '06d2b383-440d-4d15-8dd0-198c697ad364',
+        payload: {
+          user_id: 333442,
+          channel_id: 1087064,
+          user_login: 'idaspin_channel'
+        },
+        message: 'подписался на канал!'
+    };
+});
+
+WASD.event.on('subscribe', (event) => {
+    let event_example = {
+        "channel_id": 1087064,
+        "other_roles": [],
+        "product_name": "subscription_v2",
+        "product_code": "subscription_v2",
+        "user_id": 333442,
+        "user_login": "idoaspin",
+        "validity_months": 1
+    };
+});
+
+WASD.event.on('error', (label, err) => {
+});
